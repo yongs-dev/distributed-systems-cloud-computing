4bb2c421240b0408ee23ebe7b309ce470cb65b78
Ethan <yongseok993@gmail.com>
2025-01-07 11:06:21 +0900

    #. Distributed-Search - BackEnd

---
+++ distributed-search-frontend/src/main/java/Application.java
@@ -0,0 +1,66 @@
+import cluster.management.ServiceRegistry;
+import networking.WebServer;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import search.UserSearchHandler;
+
+import java.io.IOException;
+
+public class Application implements Watcher {
+    private static final String ZOOKEEPER_ADDRESS = "localhost:2181";
+    private static final int SESSION_TIMEOUT = 3000;
+    private ZooKeeper zooKeeper;
+
+    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
+        int currentServerPort = 9000;
+        if (args.length == 1) {
+            currentServerPort = Integer.parseInt(args[0]);
+        }
+        Application application = new Application();
+        ZooKeeper zooKeeper = application.connectToZookeeper();
+
+        ServiceRegistry coordinatorsServiceRegistry = new ServiceRegistry(zooKeeper, ServiceRegistry.COORDINATORS_REGISTRY_ZNODE);
+
+        UserSearchHandler searchHandler = new UserSearchHandler(coordinatorsServiceRegistry);
+        WebServer webServer = new WebServer(currentServerPort, searchHandler);
+        webServer.startServer();
+
+        System.out.println("Server is listening on port " + currentServerPort);
+
+        application.run();
+        application.close();
+        System.out.println("Disconnected from Zookeeper, exiting application");
+    }
+
+    public ZooKeeper connectToZookeeper() throws IOException {
+        this.zooKeeper = new ZooKeeper(ZOOKEEPER_ADDRESS, SESSION_TIMEOUT, this);
+        return zooKeeper;
+    }
+
+    public void run() throws InterruptedException {
+        synchronized (zooKeeper) {
+            zooKeeper.wait();
+        }
+    }
+
+    public void close() throws InterruptedException {
+        zooKeeper.close();
+    }
+
+    @Override
+    public void process(WatchedEvent event) {
+        switch (event.getType()) {
+            case None:
+                if (event.getState() == Event.KeeperState.SyncConnected) {
+                    System.out.println("Successfully connected to Zookeeper");
+                } else {
+                    synchronized (zooKeeper) {
+                        System.out.println("Disconnected from Zookeeper event");
+                        zooKeeper.notifyAll();
+                    }
+                }
+        }
+    }
+}
---
+++ distributed-search-frontend/src/main/java/cluster/management/ServiceRegistry.java
@@ -0,0 +1,113 @@
+package cluster.management;
+
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class ServiceRegistry implements Watcher {
+    public static final String WORKERS_REGISTRY_ZNODE = "/workers_service_registry";
+    public static final String COORDINATORS_REGISTRY_ZNODE = "/coordinators_service_registry";
+    private final ZooKeeper zooKeeper;
+    private List<String> allServiceAddresses = null;
+    private String currentZnode = null;
+    private final String serviceRegistryZnode;
+    private final Random random;
+
+    public ServiceRegistry(ZooKeeper zooKeeper, String serviceRegistryZnode) {
+        this.zooKeeper = zooKeeper;
+        this.serviceRegistryZnode = serviceRegistryZnode;
+        this.random = new Random();
+        createServiceRegistryNode();
+    }
+
+    public void registerToCluster(String metadata) throws KeeperException, InterruptedException {
+        this.currentZnode = zooKeeper.create(serviceRegistryZnode + "/n_", metadata.getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+        System.out.println("Registered to service registry");
+    }
+
+    public void registerForUpdates() {
+        try {
+            updateAddresses();
+        } catch (KeeperException e) {
+        } catch (InterruptedException e) {
+        }
+    }
+
+    public void unregisterFromCluster() {
+        try {
+            if (currentZnode != null && zooKeeper.exists(currentZnode, false) != null) {
+                zooKeeper.delete(currentZnode, -1);
+            }
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createServiceRegistryNode() {
+        try {
+            if (zooKeeper.exists(serviceRegistryZnode, false) == null) {
+                zooKeeper.create(serviceRegistryZnode, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized List<String> getAllServiceAddresses() throws KeeperException, InterruptedException {
+        if (allServiceAddresses == null) {
+            updateAddresses();
+        }
+        return allServiceAddresses;
+    }
+
+    public synchronized String getRandomServiceAddress() throws KeeperException, InterruptedException {
+        if (allServiceAddresses == null) {
+            updateAddresses();
+        }
+        if (!allServiceAddresses.isEmpty()) {
+            int randomIndex = random.nextInt(allServiceAddresses.size());
+            return allServiceAddresses.get(randomIndex);
+        } else {
+            return null;
+        }
+    }
+
+    private synchronized void updateAddresses() throws KeeperException, InterruptedException {
+        List<String> workers = zooKeeper.getChildren(serviceRegistryZnode, this);
+
+        List<String> addresses = new ArrayList<>(workers.size());
+
+        for (String worker : workers) {
+            String serviceFullpath = serviceRegistryZnode + "/" + worker;
+            Stat stat = zooKeeper.exists(serviceFullpath, false);
+            if (stat == null) {
+                continue;
+            }
+
+            byte[] addressBytes = zooKeeper.getData(serviceFullpath, false, stat);
+            String address = new String(addressBytes);
+            addresses.add(address);
+        }
+
+        this.allServiceAddresses = Collections.unmodifiableList(addresses);
+        System.out.println("The cluster addresses are: " + this.allServiceAddresses);
+    }
+
+    @Override
+    public void process(WatchedEvent event) {
+        try {
+            updateAddresses();
+        } catch (KeeperException e) {
+        } catch (InterruptedException e) {
+        }
+    }
+}
